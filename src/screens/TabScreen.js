d75ccddee5e4d76cbbe955878f22d2d4eefa679a
Aarav Kapoor <aaravkapoor1221@gmail.com>
2026-03-14 16:12:02 +0530

    Add files via upload

---
+++ src/screens/TabScreen.js
@@ -0,0 +1,31 @@
+import { Body, Container, Header, Left, Right, Tab, Tabs, Title } from 'native-base';
+import { Component } from 'react';
+import Tab1 from './tabs/tab1';
+import Tab2 from './tabs/tab2';
+import Tab3 from './tabs/tab3';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header style={{backgroundColor:'#009387'}} hasTabs>
+            <Left/>
+          <Body>
+            <Title style={{color:'white'}}>News App</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs tabBarUnderlineStyle={{backgroundColor:'white'}}>
+          <Tab tabStyle={{backgroundColor:'#009387'}} activeTabStyle={{backgroundColor:'#009387'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="General">
+            <Tab1 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#009387'}} activeTabStyle={{backgroundColor:'#009387'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="Business">
+            <Tab2 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#009387'}} activeTabStyle={{backgroundColor:'#009387'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="Technology">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
